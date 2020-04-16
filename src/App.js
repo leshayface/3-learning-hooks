d8015f08747eaa7aeffc8aaf0312e9aa12ad62a9
leshayface <leshaywebdzn@gmail.com>
2020-04-16 13:44:24 +0200

    upload app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [person, setPerson] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getPerson();
+  }, [])
+
+  const getPerson = async () => {
+    const response = await fetch("https://api.randomuser.me/");
+    const data = await response.json();
+    const item = data.results[0].name.first;
+    setPerson(item);
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked the counter {counter} times</p>
+      <button onClick={() => (setCounter(counter +1))}>Click!!</button>
+      <h1>My Person</h1>
+      {loading ? "Loading..." : <p><strong>Name: </strong>{person}</p>}
     </div>
   );
 }
